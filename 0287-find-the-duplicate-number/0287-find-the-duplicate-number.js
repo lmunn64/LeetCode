893f4af563e259046613b7f1b112d1250f69ed57
Luke Munn <145295385+lmunn64@users.noreply.github.com>
2025-01-16 15:35:01 -0600

    Time: 8 ms (61.27%), Space: 58.4 MB (65.55%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -1,23 +1,35 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var findDuplicate = function(nums) {
-
-// };
-
 /**
- * Map solution DP O(n) O(n)
  * @param {number[]} nums
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    var hash = new Set()
-    for(var i = 0; i < nums.length; i++){
-        if(hash.has(nums[i])){
-            return nums[i]
-        }
-        hash.add(nums[i], 1)
+    var slow = nums[0]
+    var fast = nums[nums[0]]
+
+    while(fast !== slow){
+        slow = nums[slow]
+        fast = nums[nums[fast]]
     }
-    return -1
+    var slow2 = 0
+    while(slow !== slow2){
+        slow = nums[slow]
+        slow2 = nums[slow2]
+    }
+    return slow
 };
+
+// /**
+//  * Map solution DP O(n) O(n)
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var findDuplicate = function(nums) {
+//     var hash = new Set()
+//     for(var i = 0; i < nums.length; i++){
+//         if(hash.has(nums[i])){
+//             return nums[i]
+//         }
+//         hash.add(nums[i], 1)
+//     }
+//     return -1
+// };
