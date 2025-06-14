017a8fd025c93a251d8a8e17acbbc1c57e17702e
Luke Munn <145295385+lmunn64@users.noreply.github.com>
2025-06-14 13:22:09 -0500

    Time: 17 ms (37.61%), Space: 61.2 MB (9.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(hm.containsKey(nums[i])){
+                return true;
+            }
+            hm.put(nums[i], i);
+        }
+        return false;
+    }
+}
